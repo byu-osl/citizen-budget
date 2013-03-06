d229fb9cee67eb0169f8b2655c22558ac5d0d523
ecl <eclazalde@gmail.com>
2013-03-06 14:10:38 -0700

    Changed the name of the parser file

--- financialParsers/financialsParser.py
+++ financialParsers/financialsParser.py
@@ -10,8 +10,9 @@ import sys
 # write the data into the appropriate csv file to be collected.
 ###############################################################################
 def main(filename):
-  fundsFile = open("fundsFile.csv", "w")
-  breakdownFile = open("breakdownFile.csv", "w")
+  fileDate = filename[:-4]
+  fundsFile = open(fileDate + "-fundsFile.csv", "w")
+  breakdownFile = open(fileDate + "-breakdownFile.csv", "w")
   
   with open(filename, 'rb') as csvfile:
   
@@ -24,40 +25,36 @@ def main(filename):
     
     for row in reader:
       if (len(row) > 1):
-        if not (wrongTotals(row[0])):
-          if row[0].startswith ("TOTAL"):
-          
-            ytdActual = row[2].replace(",", "")
-            budgeted = row[3].replace(",", "")
-            currentCatagory = row[0][6:]
-            #TODO break these into functions
-            if currentCatagory == "FUND REVENUE":
-              isExpense = True
-              budgetedRevenue = budgeted
-              fundsFile.write(ytdActual + ", ")
-            elif currentCatagory == "FUND EXPENDITURES":
-              isExpense = False
-              fundsFile.write(ytdActual + ", " + budgetedRevenue + ", "+ budgeted +", ")
+        if row[0].startswith ("TOTAL") and not (wrongTotals(row[0])):
+          ytdActual = row[2].replace(",", "")
+          budgeted = row[3].replace(",", "")
+          currentCatagory = row[0][6:]
+          #TODO break these into functions
+          if currentCatagory == "FUND REVENUE":
+            isExpense = True
+            budgetedRevenue = budgeted
+            fundsFile.write(ytdActual + ", ")
+          elif currentCatagory == "FUND EXPENDITURES":
+            isExpense = False
+            fundsFile.write(ytdActual + ", " + budgetedRevenue + ", "+ budgeted +", ")
+          else:
+            if isExpense:
+              breakdownType = "expense, "
             else:
-              if isExpense:
-                breakdownType = "expense, "
-              else:
-                breakdownType = "revenue, "
-              breakdownFile.write(currentCatagory + ", " + fundName + ", " + date + ", " + breakdownType + ytdActual +", " + budgeted + ", \n")
-            
-          if row[0].startswith ("NET"):
-            #TODO put this in a function
-            currentCatagory = row[0][4:] # Get Net Name
-            netBudget = row[2]
-            netBudget = buildNetBudget(netBudget)
-            fundsFile.write(netBudget + ", \n")
+              breakdownType = "revenue, "
+            breakdownFile.write(currentCatagory + ", " + fundName + ", " + date + ", " + breakdownType + ytdActual +", " + budgeted + ", \n")
+        if row[0].startswith ("NET") and not (wrongTotals(row[0])):
+          #TODO put this in a function
+          currentCatagory = row[0][4:] # Get Net Name
+          netBudget = row[2]
+          netBudget = buildNetBudget(netBudget)
+          fundsFile.write(netBudget + ", \n")
         
       if (len(row) == 1):
         #collects and formats the date if necessary
         if needDate and isDate(row[0]):
           date = makeDate(row[0])
           needDate = False
-          
         if row[0].endswith ("FUND"):
           if fundName == row[0]:
             pass
@@ -76,7 +73,7 @@ def makeDate(date):
   return time.strftime("%m/%d/%Y",conv)
     
 ###############################################################################
-# Fixes Net Budget because there are () instead of -
+# Removes parenthesis for negative numbers and adds a negative sign
 ###############################################################################
 def buildNetBudget(netBudget):
   netBudget = netBudget.replace(",", "")
@@ -86,7 +83,7 @@ def buildNetBudget(netBudget):
   return netBudget
         
 ###############################################################################
-# Finds the date by checking for every month in the text
+# Looks at the line to see if the first word is a month of the year.
 ###############################################################################
 def isDate(firstEntry):
   months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", 
