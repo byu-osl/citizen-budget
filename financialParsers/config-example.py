c4c711b6aae5f5e329f08eee4aae79d73df3bac9
Daniel Zappala <daniel.zappala@gmail.com>
2013-06-30 15:32:55 -0600

    Removed old code.

--- financialParsers/config-example.py
+++
@@ -1,9 +0,0 @@
-# Configuration for uploading to Google Fusion table
-
-CLIENT_ID     = ''
-CLIENT_SECRET = ''
-SCOPE         = 'https://www.googleapis.com/auth/fusiontables'
-
-# (Fund Table, Fund BreakDown Category)
-TABLE_ID      = ("",
-                 "")
